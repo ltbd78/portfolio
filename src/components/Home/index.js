b1a4cfd8707c9580ef576c947ad8bc1bc21e8c46
Linsu Han <ltbd78@gmail.com>
2023-02-12 18:08:43 -0500

    30:51

---
+++ src/components/Home/index.js
@@ -0,0 +1,23 @@
+import LogoTitle from '../../assets/images/logo-l.png'
+import { Link } from 'react-router-dom'
+import './index.scss';
+
+const Home = () => {
+    return (
+        <div className='container home-page'>
+            <div className="text-zone">
+                <h1>
+                    Hi,
+                    <br />
+                    I'm <img src={LogoTitle} alt='logo' />insu Han,
+                    <br />
+                    Data Scientist.
+                </h1>
+                <h2>Machine Learning Engineer / Statistician / TikToker</h2>
+                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+            </div>
+        </div>
+    )
+}
+
+export default Home
