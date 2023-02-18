6d09ba24ada0ff643c7cdf1f0a01e86a7637f80c
Linsu Han <ltbd78@gmail.com>
2023-02-18 17:39:30 -0500

    36:22

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,15 +1,22 @@
 import LogoTitle from '../../assets/images/logo-l.png'
 import { Link } from 'react-router-dom'
 import './index.scss';
+import AnimatedLetters from '../AnimatedLetters';
+import { useState } from 'react'
 
 function Home() {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['i', 'n', 's', 'u', 'h', 'a', 'n']
+    const jobArray = ['d', 'a', 't', 'a', 's', 'c', 'i', 'e', 'n', 't', 'i', 's', 't']
+
     return (
         <div className='container home-page'>
             <div className="text-zone">
                 <h1>
                     Hi,
                     <br />
-                    I'm<img src={LogoTitle} alt='logo' />insu Han,
+                    I'm<img src={LogoTitle} alt='logo' />
+                    <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15} />
                     <br />
                     Data Scientist.
                 </h1>
