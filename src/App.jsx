361d288dbf180a956cf5d57ec1e779fd57461276
Linsu Han <ltbd78@gmail.com>
2023-03-24 16:31:17 -0400

    change default router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.scss";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { MemoryRouter, Routes, Route } from "react-router-dom";
 import About from "./components/About";
 import Home from "./components/Home";
 import Layout from "./components/Layout";
@@ -7,15 +7,16 @@ import Layout from "./components/Layout";
 // https://github.com/bobangajicsm/react-portfolio-website
 // https://create-react-app.dev/docs/deployment/#github-pages
 // https://sombriks.com/blog/0044-cra-builds-for-non-root-urls
+// https://stackoverflow.com/a/36623117/6535624 // basename={process.env.REACT_APP_BASENAME}
 export default function App() {
   return (
-    <BrowserRouter basename={process.env.REACT_APP_BASENAME}>
+    <MemoryRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </MemoryRouter>
   );
 }
