628e8d32189f6a897277b0625098107ea51b2369
Linsu Han <ltbd78@gmail.com>
2023-04-27 11:36:25 -0400

    update

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -11,34 +11,37 @@ export default function Navbar() {
   return (
     <div className="nav-bar">
       <img src={LogoL} className="logo" alt="logo" />
-      <nav>
-        <NavLink className="home-link" to="/">
-          <FontAwesomeIcon icon={faHome} />
-          <span className="link-text">HOME</span>
-        </NavLink>
-        <NavLink className="about-link" to="about">
-          <FontAwesomeIcon icon={faUser} />
-          <span className="link-text">ABOUT</span>
-        </NavLink>
-        <NavLink className="contact-link" to="contact">
-          <FontAwesomeIcon icon={faEnvelope} />
-          <span className="link-text">CONTACT</span>
-        </NavLink>
-      </nav>
-      <nav>
-        <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/linsuhan/">
-          <FontAwesomeIcon icon={faLinkedin} />
-        </a>
-        <a target="_blank" rel="noreferrer" href="https://www.github.com/ltbd78/">
-          <FontAwesomeIcon icon={faGithub} />
-        </a>
-        <a target="_blank" rel="noreferrer" href="https://www.instagram.com/ltbd78/">
-          <FontAwesomeIcon icon={faInstagram} />
-        </a>
-        <a target="_blank" rel="noreferrer" href="https://www.tiktok.com/@linsuhan/">
-          <FontAwesomeIcon icon={faTiktok} />
-        </a>
-      </nav>
+      <div className="nav-links">
+        <div className="logo-placeholder"></div>
+        <nav>
+          <NavLink className="home-link" to="/">
+            <FontAwesomeIcon icon={faHome} />
+            <span className="link-text">HOME</span>
+          </NavLink>
+          <NavLink className="about-link" to="about">
+            <FontAwesomeIcon icon={faUser} />
+            <span className="link-text">ABOUT</span>
+          </NavLink>
+          <NavLink className="contact-link" to="contact">
+            <FontAwesomeIcon icon={faEnvelope} />
+            <span className="link-text">CONTACT</span>
+          </NavLink>
+        </nav>
+        <nav>
+          <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/linsuhan/">
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+          <a target="_blank" rel="noreferrer" href="https://www.github.com/ltbd78/">
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <a target="_blank" rel="noreferrer" href="https://www.instagram.com/ltbd78/">
+            <FontAwesomeIcon icon={faInstagram} />
+          </a>
+          <a target="_blank" rel="noreferrer" href="https://www.tiktok.com/@linsuhan/">
+            <FontAwesomeIcon icon={faTiktok} />
+          </a>
+        </nav>
+      </div>
       <FontAwesomeIcon onClick={() => setShowNav(true)} icon={faBars} className="hamburger-icon" />
     </div>
   );
