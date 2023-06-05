0b7bc11ea41ebd555fe8f3a1ea6a053f5093e33f
ltbd78 <ltbd78@gmail.com>
2023-06-05 19:21:57 -0400

    save point

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -2,13 +2,18 @@ import "./index.scss";
 import { useState } from "react";
 import AnimatedLetters from "../AnimatedLetters";
 import portfolioGIF from "../../assets/portfolio/portfolio.gif";
+import RLGIF from "../../assets/portfolio/rl.gif";
 
 function Work({ href, src, title, description }) {
+  // if src is none use alternative text
   return (
     <a className="work" href={href} target="_blank">
-      <img src={src} alt="portfolio-work"></img>
-      <div className="title">{title}</div>
-      <div className="description">{description}</div>
+      <img src={src} alt="portfolio-work" />
+      <div className="text-box">
+        <div className="blank"></div>
+        <div className="title">{title}</div>
+        <div className="description">{description}</div>
+      </div>
     </a>
   );
 }
@@ -24,12 +29,14 @@ export default function Portfolio() {
         <h1>
           <AnimatedLetters strArray={"Portfolio".split("")} initialClass={initialClass} delay={10} hoverClass={hoverClass} />
         </h1>
-        <img className="portfolio-GIF" src={portfolioGIF} alt="portfolio-GIF"></img>
+        <div className="image-container">
+          <img className="portfolio-GIF" src={portfolioGIF} alt="portfolio-GIF"/>
+        </div>
       </div>
       <div className="works">
         <Work
           href={"https://github.com/ltbd78/RL"}
-          src={portfolioGIF}
+          src={RLGIF}
           title={"Flexible Reinforcement Learning Framework"}
           description={"A flexible reinforcement learning framework that allows easy mix and matching of different agents and environments."}
         />
