6675ef6dd80cb4065cc74000b0b96362ab06f4ed
ltbd78 <ltbd78@gmail.com>
2023-06-20 16:16:53 -0400

    final touches

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -1,14 +1,23 @@
 import "./index.scss";
 import { useState } from "react";
 import AnimatedLetters from "../AnimatedLetters";
+import caePNG from "../../assets/portfolio/cae.png";
+import celebaGIF from "../../assets/portfolio/celeba.gif";
 import portfolioGIF from "../../assets/portfolio/portfolio.gif";
 import RLGIF from "../../assets/portfolio/rl.gif";
+import siteGIF from "../../assets/portfolio/site.gif";
 
-function Work({ href, src, title, description }) {
+function Work({ href, src, title, description, width, height, transform }) {
   // target="_blank" opens link in new tab
+  const imgStyle = {
+    width: width,
+    height: height,
+    transform: transform
+  };
+
   return (
     <a className="work" href={href} target="_blank">
-      <img src={src} alt=" " />
+      <img src={src} alt=" " style={imgStyle} />
       <div className="text-box">
         <div className="title">{title}</div>
         <div className="description">{description}</div>
@@ -18,7 +27,7 @@ function Work({ href, src, title, description }) {
 }
 
 export default function Portfolio() {
-  // TODO: do a see more
+  // TODO: add a see more button
   const [initialClass, setInitialClass] = useState("initial-animation");
   const [hoverClass, setHoverClass] = useState("");
   setTimeout(() => setInitialClass(""), 2000);
@@ -39,12 +48,35 @@ export default function Portfolio() {
           src={RLGIF}
           title={"Flexible Reinforcement Learning Framework"}
           description={"A flexible reinforcement learning framework that allows easy mix and matching of different agents and environments."}
+          width={"100%"}
+          height={"100%"}
         />
-        <Work />
-        <Work />
-        <Work />
-        <Work />
-        <Work />
+        <Work
+          href={"https://github.com/ltbd78/facial-feature-detection"}
+          src={celebaGIF}
+          title={"Facial Feature Detection"}
+          description={"Flask hosted, real-time face detection & feature classification; ResNet model trained on CelebA dataset."}
+          width={"200%"}
+          height={"150%"}
+          transform={"translateY(-13%) translateX(-5%)"}
+        />
+        <Work
+          href={"https://github.com/ltbd78/cae"}
+          src={caePNG}
+          title={"Convolutional Autoencoder"}
+          description={"Convolutional autoencoder used for image reconstruction and anomaly detection."}
+          width={"100%"}
+          height={"100%"}
+        />
+        <Work
+          href={"https://github.com/ltbd78/portfolio"}
+          src={siteGIF}
+          title={"Personal Portfolio Website"}
+          description={"You're looking at the website right now! Click to see source code."}
+          width={"100%"}
+          height={"130%"}
+          transform={"translateY(10%)"}
+        />
       </div>
     </article>
   );
