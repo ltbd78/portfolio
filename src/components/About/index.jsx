9ffaedb7015f3d8894230cde17c18c3f4c1fbe40
Linsu Han <ltbd78@gmail.com>
2024-05-25 14:16:11 -0400

    update About paragraphs

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -47,21 +47,13 @@ export default function About() {
           <Cube />
         </div>
         <p>
-          Hello, I'm a data scientist currently living in NYC. I was born in Beijing, China, but flew to America when I was just three years old. My passion
-          for numbers and patterns led me to study math and statistics for my bachelor's degree at Purdue University. After that, I pursued my master's
-          degree in statistics at Columbia University, which paved the way for my career in data science.
+          Born in Beijing, China, I moved to the United States at the age of three and have lived here ever since. I completed my undergraduate studies at Purdue University, where I initially pursued a prepharmacy track but eventually discovered my passion for mathematics and statistics, earning a bachelor's degree in these fields. My academic journey continued at Columbia University, where I obtained a master's degree in Statistics. Currently, I reside in the vibrant city of New York.
         </p>
         <p>
-          Before joining Capital One, I worked as a machine learning engineer in Boston, where I conducted research on deep learning. The experience helped
-          me develop a strong foundation in machine learning algorithms, which I now use to derive insights and make data-driven decisions at my current
-          job. I enjoy coding and learning new things, which has enabled me to stay on top of the latest developments in my field.
+          My professional career has spanned various roles and industries, all centered around machine learning and data science. I began as a Deep Learning Researcher at a startup in Boston, focusing on reinforcement learning and computer vision. I then moved to Capital One as a Senior Data Scientist, where I worked on developing recommendation systems. My journey continued at Walmart, where I served as a Senior Machine Learning Engineer, delving into the realm of generative AI. Currently, I am a Senior Machine Learning Engineer at JPMorgan Chase, specializing in large language models (LLMs) and retrieval-augmented generation (RAG).
         </p>
         <p>
-          When I'm not working, I enjoy exploring my creative side through various hobbies. I've been playing the violin since I was young, and it remains a
-          passion of mine to this day. I also enjoy watching anime and discovering new series to immerse myself in. Additionally, I have a love for hip hop
-          dance, which not only helps me stay in shape but also allows me to express myself creatively. Lastly, I am an avid gamer, both competitively and
-          for fun. I enjoy games like League of Legends, where I've achieved a ranking in the top 0.1%, as well as RPGs such as Genshin Impact, Lost Ark,
-          and Guild Wars 2. These hobbies allow me to exercise my brain in different ways and provide a much-needed break from my day-to-day work.
+          Outside of my professional life, I am an avid learner, always eager to explore new subjects and ideas. I have a deep appreciation for music and play the violin. My eclectic tastes extend to anime and a love for good music, particularly hip hop and break dancing. Additionally, I am a competitive League of Legends player, ranking in the top 0.1%, which reflects my dedication and strategic thinking both in and out of the game. [Written by ChatGPT]
         </p>
       </article>
     </>
@@ -69,19 +61,23 @@ export default function About() {
 }
 
 /*
-CHATGPT Prompt:
-Given some descriptors of me below, write me a first-person "About Me" section of exactly 3 paragraphs long. The order I listed is arbitrary, feel free to reorder however you want such that it's fluid:
+Background
 - born in beijing, china; flew to america when I was 3
-- current living in NYC working as data scientist at capital one 
-- previous job, machine learning engineer at Boston; deep learning research
-- bachelor's degree: math and statistics at purdue university, 
-- master's degree: Statistics at columbia university
-- like coding, learning new things
-- hobbies:
-  a. violin
-  b. anime
-  c. hip hop dance
-  d. video games:
-    1. competitve (i.e. league of legends (peaked top 0.1%))
-    2. rpgs (i.e. genshin impact, lost ark, guild wars 2)
+- prepharmacy but switched and graduated w/ bachelor's degree: math and statistics at Purdue university
+- master's degree: Statistics at Columbia University
+- current living and working in NYC
+
+Career:
+- Deep Learning Researcher at a startup in Boston; reinforcement learning, computer vision
+- Data Scientist at Capital One; recommendation systems
+- Machine Learning Engineer at Walmart; generative ai
+- Machine Learning Engineer at JPMorgan Chase; LLMs and RAG
+
+Hobbies:
+- learning new things
+- violin
+- anime
+- good music
+- hip hop and break dance
+- competitive League of Legends (top 0.1%)
 */
